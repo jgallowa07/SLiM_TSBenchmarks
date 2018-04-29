64b53ddd2669fad317858b1e879bcd3e749cda98
jgallowa07 <jaredgalloway07@gmail.com>
2018-04-28 20:54:19 -0700

    script for running sims on lab computer

--- Bench/scripts/example_bash_script.sh
+++ Bench/scripts/example_bash_script.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-SEED=$(printf "%06d" $RANDOM);
-echo $SEED
+SEED=27467
+N=10
+size=10
 
-RUNNUM = $RUN_NUMBER
+TIME_MEM_FILE_N=neutral_time_arg.N$N"."size$size".out"
+TIME_MEM_FILE_I=initial_pedigree_time_arg.N$N"."size$size".out"
+TIME_MEM_FILE_U=unordered_map_time_arg.N$N"."size$size".out"
+
+(time ../../bin_initial/slim -s $SEED -d n_ind=$N -d size_rho=$size ../BenchRecipes/benchmarks_neutral.slim &> ../slurm_runs/$TIME_MEM_FILE_N) \
+	&> ../Optimizations/$TIME_MEM_FILE_N
 
