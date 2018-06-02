72e7de30d4707e6ed910e79668a0477727b7cdea
jared <jared@hazelnut.uoregon.edu>
2018-06-02 09:44:56 -0700

    latestTimes

--- Bench/scripts/runAll.sh
+++ Bench/scripts/runAll.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SEED=27467
-SECONDS_TO_KILL=`echo "72*60*60"|bc -l`
+#SECONDS_TO_KILL=`echo "72*60*60"|bc -l`
 
-for N in 1000 10000 
+for N in 1000
 do
 	for size in 100000  
 	do	
@@ -26,27 +26,27 @@ do
 		MUTRATE=$(echo "scale=10; ($size / (4.0 * $N)) / 100000000.0" | bc)
 
 		#Neutral Mutations
-		gtimeout $SECONDS_TO_KILL /usr/local/Cellar/gnu-time/1.9/bin/gtime --format='%E / %S / %U / %M' \
+		/usr/local/Cellar/gnu-time/1.9/bin/gtime --format='%E / %S / %U / %M' \
 		  --output="../ComparisonTimes/RealTimes/$NEUT_TIME_FILE_NAME" ../../../SLiM/bin/slim \
 		  -s $SEED -d n_ind=$N -d size_rho=$size ../BenchRecipes/benchmarks_neutral.slim &> ../Output/Reals/$NEUT_OUTPUT_FILE_NAME
 
 		#Neutral Mutations Release SLiM
-		gtimeout $SECONDS_TO_KILL /usr/local/Cellar/gnu-time/1.9/bin/gtime --format='%E / %S / %U / %M' \
+		/usr/local/Cellar/gnu-time/1.9/bin/gtime --format='%E / %S / %U / %M' \
 		  --output="../ComparisonTimes/RealTimes/$REL_TIME_FILE_NAME" ../../../SLiM2_6/bin/slim \
 		  -s $SEED -d n_ind=$N -d size_rho=$size ../BenchRecipes/benchmarks_neutral.slim &> ../Output/Reals/$REL_OUTPUT_FILE_NAME
 
 		#Pedigree Recording With Mutations
-		gtimeout $SECONDS_TO_KILL /usr/local/Cellar/gnu-time/1.9/bin/gtime --format='%E / %S / %U / %M' \
+		/usr/local/Cellar/gnu-time/1.9/bin/gtime --format='%E / %S / %U / %M' \
 		  --output="../ComparisonTimes/RealTimes/$PED_TIME_FILE_NAME" ../../../SLiM/bin/slim \
 		  -s $SEED -d n_ind=$N -d size_rho=$size ../BenchRecipes/benchmarks_pedigree.slim &> ../Output/Reals/$PED_OUTPUT_FILE_NAME
 		
 		#Time To layover neutral mutations over the trees
-		gtimeout $SECONDS_TO_KILL /usr/local/Cellar/gnu-time/1.9/bin/gtime --format="%E / %S / %U / %M" \
+		/usr/local/Cellar/gnu-time/1.9/bin/gtime --format="%E / %S / %U / %M" \
 		  --output="../ComparisonTimes/RealTimes/$PED_ADD_TIME_FILE_NAME" \
 		  /usr/local/bin/python3.6 add-mutation.py --mut_rate $MUTRATE --tree_file ../Tables/$PED_TABLE_FILE -o ../VCF_Tables/ &> ../Output/Reals/$PED_ADD_OUTPUT_FILE_NAME  
 
 		#Pedigree Recording W/O Mutations
-		gtimeout $SECONDS_TO_KILL /usr/local/Cellar/gnu-time/1.9/bin/gtime --format='%E / %S / %U / %M' \
+		/usr/local/Cellar/gnu-time/1.9/bin/gtime --format='%E / %S / %U / %M' \
 		  --output="../ComparisonTimes/RealTimes/$PEDNM_TIME_FILE_NAME" ../../../SLiM/bin/slim \
 		  -s $SEED -d n_ind=$N -d size_rho=$size ../BenchRecipes/benchmarks_pedigree_nm.slim &> ../Output/Reals/$PEDNM_OUTPUT_FILE_NAME
 
